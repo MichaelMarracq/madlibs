6685dd418789bec8afb690d44854aa23ec2b7f4f
LilMike707 <michaelmarracq@yahoo.com>
2023-10-04 16:36:08 -0700

    Update addComma.test.js

--- madlibs/addComma.test.js
+++ madlibs/addComma.test.js
@@ -4,4 +4,16 @@ describe("#addComma", () => {
   test("it is a function", () => {
     expect(typeof addComma).toBe("function");
   });
+
+  test("it adds commas to an integer", () => {
+    expect(addComma(123456789)).toBe("123,456,789");
+  });
+
+  test("it adds commas to a decimal number", () => {
+    expect(addComma(9876543.21)).toBe("9,876,543.21");
+  });
+
+  test("it adds commas to a large integer", () => {
+    expect(addComma(1234567890123456)).toBe("1,234,567,890,123,456");
+  });
 });
